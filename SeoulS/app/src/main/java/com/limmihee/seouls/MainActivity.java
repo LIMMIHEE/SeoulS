cd0bb5ef3b58a57438f6d8697fae747dae88b5b2
mirimhee <48482259+LIMMIHEE@users.noreply.github.com>
2020-03-12 01:30:51 +0900

    .

---
+++ SeoulS/app/src/main/java/com/limmihee/seouls/MainActivity.java
@@ -0,0 +1,22 @@
+package com.limmihee.seouls;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.GradientDrawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+//    ImageView imageView = (ImageView) findViewById(R.id.CardImage);
+//    GradientDrawable drawable= (GradientDrawable) getApplicationContext().getDrawable(R.drawable.card_corner_radius);
+//    imageView.setBackground(drawable);
+//    imageView.setClipToOutline(true);
+
+}
