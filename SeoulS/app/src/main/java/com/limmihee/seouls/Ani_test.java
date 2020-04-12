0c384628672395f6343ba2e05ea0c552b3dedb25
mirimhee <48482259+LIMMIHEE@users.noreply.github.com>
2020-04-12 15:39:23 +0900

    애니메이션

---
+++ SeoulS/app/src/main/java/com/limmihee/seouls/Ani_test.java
@@ -0,0 +1,14 @@
+package com.limmihee.seouls;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ani_test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ani_test);
+    }
+}
