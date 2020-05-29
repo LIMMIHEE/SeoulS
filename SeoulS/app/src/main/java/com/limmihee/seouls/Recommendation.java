99d6916ec41dd0b6deed85e368ffbc78d0440d60
LIMMIHEE <48482259+LIMMIHEE@users.noreply.github.com>
2020-05-29 09:58:25 +0900

    추천 페이지 제작 중

--- SeoulS/app/src/main/java/com/limmihee/seouls/Recommendation.java
+++ SeoulS/app/src/main/java/com/limmihee/seouls/Recommendation.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class All_sports extends AppCompatActivity {
+public class Recommendation extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_all_sports);
+        setContentView(R.layout.activity_recommendation);
     }
 }
