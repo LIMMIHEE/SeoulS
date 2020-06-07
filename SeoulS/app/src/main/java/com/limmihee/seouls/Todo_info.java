3e0cc8ed6bbc62d30052545e5c4e044ecca13588
mirimhee <48482259+LIMMIHEE@users.noreply.github.com>
2020-06-07 16:36:01 +0900

    TODO 디테일

---
+++ SeoulS/app/src/main/java/com/limmihee/seouls/Todo_info.java
@@ -0,0 +1,14 @@
+package com.limmihee.seouls;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Todo_info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo_info);
+    }
+}
